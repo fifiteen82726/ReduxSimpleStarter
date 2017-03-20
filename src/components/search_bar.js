7cfe77da34ff1e4b27e3f443bdb4622f427b9ab6
fifiteen82726 <fifiteen82726@gmail.com>
2017-03-21 00:39:31 +0800

    加上 onInputChange

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom';
 
 class SearchBar extends React.Component{
   render(){
-    return <input />;
+    return <input onChange = {this.onInputChange}/>;
+  }
+  onInputChange(event){
+    console.log(event.target.value);
   }
 };
 
