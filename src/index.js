6dee0e35ec6ba1e4c2bd0055f18fad5a3e6471ac
fifiteen82726 <fifiteen82726@gmail.com>
2017-03-24 01:14:39 +0800

    加上 API Key, youtube npm

--- src/index.js
+++ src/index.js
@@ -2,10 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 
+const API_KEY = 'AIzaSyDLhJQ0n2_3HXZLjLqN88_X2YNyWI94430';
+
+
 const App = () => { //ES6
-  return <div>
-  <SearchBar />
-   </div>;
+  return
+  <div>
+   <SearchBar />
+  </div>;
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
