93f38ff4c7a33132db2a60b3cccb6aaaabc68a42
fifiteen82726 <fifiteen82726@gmail.com>
2017-03-30 00:56:03 +0800

    排版 加上 ()

--- src/index.js
+++ src/index.js
@@ -6,9 +6,11 @@ const API_KEY = 'AIzaSyDLhJQ0n2_3HXZLjLqN88_X2YNyWI94430';
 
 
 const App = () => { //ES6
-  return <div>
-   <SearchBar />
-  </div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
