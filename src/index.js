fb3fa9793b979c5a380e03a18b3e67b2dafcebe1
fifiteen82726 <fifiteen82726@gmail.com>
2017-03-30 01:05:39 +0800

    call youtube API example

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDLhJQ0n2_3HXZLjLqN88_X2YNyWI94430';
 
+YTSearch({key: API_KEY, term: 'fuckyou'}, function(data){
+  console.log(data);
+});
 
 const App = () => { //ES6
   return (
