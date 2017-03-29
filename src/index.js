1af6fca61db5e78227094089ef2769b25b78275f
fifiteen82726 <fifiteen82726@gmail.com>
2017-03-30 01:11:47 +0800

    從const variable 換成 class function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
 
 const API_KEY = 'AIzaSyDLhJQ0n2_3HXZLjLqN88_X2YNyWI94430';
 
-YTSearch({key: API_KEY, term: 'fuckyou'}, function(data){
+YTSearch({key: API_KEY, term: 'nba'}, function(data){
   console.log(data);
 });
 
-const App = () => { //ES6
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+// 要用 State的話要換成 class function
+// const App = () => { //ES6
+class App extends Component {
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
