92b1ad1e400901f2a540b5475352219a105d5f10
fifiteen82726 <fifiteen82726@gmail.com>
2017-03-31 01:04:42 +0800

    use props to create Videolist, finish 23

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list'
 
 const API_KEY = 'AIzaSyDLhJQ0n2_3HXZLjLqN88_X2YNyWI94430';
 
@@ -13,7 +14,7 @@ class App extends Component {
 
     this.state = { videos: [] };
 
-    YTSearch({key: API_KEY, term: 'nba'}, function(videos) {
+    YTSearch({key: API_KEY, term: 'nba'}, (videos) => {
       this.setState({ videos });
     });
   }
@@ -22,6 +23,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos = {this.state.videos}/>
       </div>
     );
   }
